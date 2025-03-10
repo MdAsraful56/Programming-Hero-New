74ccb49dcfc1392fdc006e8cede3da2769d88e16
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-10 06:58:38 +0600

    i use first time mongoDB

--- Module - 55/simple-crud-client/src/App.jsx
+++ Module - 55/simple-crud-client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
 
   const handleSubmit = e => {
-    
+
     e.preventDefault()
     const form = e.target;
     const name = form.name.value;
@@ -12,7 +12,17 @@ function App() {
     const user = {name, email};
     console.log(user);
 
-
+    fetch('http://localhost:5000/users', {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      });
 
 
   }
